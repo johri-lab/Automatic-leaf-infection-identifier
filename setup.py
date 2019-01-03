7167d74229964b5d4daf3aa57068c8e46ef9f816
Pradeep Singh <pswaldia1@gmail.com>
2019-01-04 00:03:13 +0530

    Make setup.py read requirements.txt

--- setup.py
+++ setup.py
@@ -8,8 +8,11 @@ dir_path = path.abspath(path.dirname(__file__))
 # Get the long description from the README file
 with open(path.join(dir_path, 'README.md'), encoding='utf-8') as f:
 	long_description = f.read()
+	
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()	
 
-requirements = ['numpy', 'matplotlib', 'pandas', 'sklearn', 'opencv-python', 'argparse']
+#requirements will now be updated as a list which contains all the dependancies to be installed.
 
 setup(
 	name='Automatic-leaf-infection-identifier ',
